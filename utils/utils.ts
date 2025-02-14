175cf53ee38c9b4d04261036875c85a7597cfbfd
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2025-02-14 23:03:21 +0530

    reinitialised with create-next-app

--- utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export const createAvatar = (name: string) => {
-    let initials: string = "";
-    const nameSplitted: string[] = name.split(" ");
-    for (let i = 0; i < nameSplitted.length; i++) {
-      if (nameSplitted[i] == "Dr." || nameSplitted[i] == "Ms.") continue;
-      initials += nameSplitted[i][0];
-    }
-    return initials;
-}
